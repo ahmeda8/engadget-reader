2245c1f67395a7b5e0403c9b274806efbc974826
ahmeda8 <ahmeda8@outlook.com>
2012-12-27 19:31:36 -0800

    change app name

--- Engadget Reader/Properties/AssemblyInfo.cs
+++ Engadget Reader/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Resources;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Engadget_Reader")]
+[assembly: AssemblyTitle("engadget reader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Engadget_Reader")]
+[assembly: AssemblyProduct("engadget reader")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
